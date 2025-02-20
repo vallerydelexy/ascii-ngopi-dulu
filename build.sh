73abb7d8ef4b7563b8ecc4d633244ff7802e0cc2
Rizki Aprita <rizki.aprita@esign.id>
2025-02-20 14:03:33 +0700

    add gitignore

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o ascii-darwin-arm64 ascii.go
-GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ascii-darwin-amd64 ascii.go
-GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ascii-linux-amd64 ascii.go
-GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ascii-windows-amd64.exe ascii.go
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o build/ascii-darwin-arm64 ascii.go
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o build/ascii-darwin-amd64 ascii.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o build/ascii-linux-amd64 ascii.go
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o build/ascii-windows-amd64.exe ascii.go
 
 # Compress with UPX
-upx --best --ultra-brute ascii-darwin-arm64
-upx --best --ultra-brute ascii-darwin-amd64
-upx --best --ultra-brute ascii-linux-amd64
-upx --best --ultra-brute ascii-windows-amd64.exe
+upx --best --ultra-brute build/ascii-darwin-arm64
+upx --best --ultra-brute build/ascii-darwin-amd64
+upx --best --ultra-brute build/ascii-linux-amd64
+upx --best --ultra-brute build/ascii-windows-amd64.exe
